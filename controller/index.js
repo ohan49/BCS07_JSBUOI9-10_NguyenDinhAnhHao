e94acfebc421762cf92f7abbb333ac85b16e057d
AnhHao <pandalikesoda@gmail.com>
2023-06-01 00:01:53 +0700

    second commit

--- controller/index.js
+++ controller/index.js
@@ -19,11 +19,13 @@ function renderGiaoDien() {
                     }')" class="btn btn-danger">
                       <i class="fa-solid fa-trash"></i>
                     </button>
-                    <button onclick="editNhanVien('${
+                    <button onclick="capNhat('${
                       nhanVien.tK
-                    }')" class="btn btn-warning">
+                    }')" data-toggle="modal"
+                    data-target="#myModal" class="btn btn-warning">
                       <i class="fa-solid fa-pen"></i>
-                    </button></td>
+                    </button>
+                    </td>
                   </tr>
   `;
   }
@@ -54,20 +56,57 @@ function themNhanVien() {
   arrNhanVien.push(nhanVien);
   // render lên giao diện
   renderGiaoDien();
+  ganGiaTriChoInput("", "", "", "", "", "", "", "");
 }
 
 document.getElementById("btnThemNV").onclick = themNhanVien;
 
-// Xóa nhân viên
-function xoaNhanVien() {
+// Tìm vị trí nhân viên
+function timNhanVien(tK) {
   var viTri = -1;
   arrNhanVien.forEach(function (item, index) {
     if (item.tK == tK) {
       viTri = index;
     }
   });
+  return viTri;
+}
+
+// Gán giá trị
+function ganGiaTriChoInput(tK, name, email, pass, ngayLam, luong, cV, gioLam) {
+  document.getElementById("tknv").value = tK;
+  document.getElementById("name").value = name;
+  document.getElementById("email").value = email;
+  document.getElementById("password").value = pass;
+  document.getElementById("datepicker").value = ngayLam;
+  document.getElementById("luongCB").value = luong;
+  document.getElementById("chucvu").value = cV;
+  document.getElementById("gioLam").value = gioLam;
+}
+
+// Xóa nhân viên
+function xoaNhanVien(tK) {
+  console.log(tK);
+  var index = timViTriNhanVien(tK);
   if (viTri != -1) {
-    arrNhanVien.slice(viTri, 1);
+    arrNhanVien.splice(viTri, 1);
     renderGiaoDien();
   }
 }
+
+// Cập nhập nhân viên
+function capNhat(tK) {
+  var index = timViTriNhanVien(tK);
+  var nhanVien = arrNhanVien[index];
+  ganGiaTriChoInput(
+    nhanVien.tK,
+    nhanVien.name,
+    nhanVien.email,
+    nhanVien.pass,
+    nhanVien.ngayLam,
+    nhanVien.luong,
+    nhanVien.cV,
+    nhanVien.gioLam
+  );
+}
+document.getElementById("btnCapNhat").onclick = capNhat;
